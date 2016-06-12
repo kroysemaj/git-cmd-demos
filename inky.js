2e56cd8ea5c1e2089f3c6e5badf3bf5eb816c69a
James York <james@grandcircus.co>
2016-06-12 14:13:05 -0400

    Revert "resolved merge conflicts""
    This reverts commit 89af68fc789da0e640ad491ad76e5b32d5629a3d.

--- inky.js
+++ inky.js
@@ -1,3 +1,7 @@
+<p>"He sleeps in his boots, don't he? He hasn't got any hammock; but I've seen him lay of nights in a coil of rigging."</p>
+
+<p>"No doubt, and it's because of his cursed tail; he coils it down, do ye see, in the eye of the rigging."</p>
+
 <p>"What's the old man have so much to do with him for?"</p>
 
 <p>"Striking up a swap or a bargain, I suppose."</p>
