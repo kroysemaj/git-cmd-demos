1c2394d657e426c9843073a287e056c4bc8bb237
James York <james@grandcircus.co>
2016-06-12 14:08:45 -0400

    commit 3

--- inky.js
+++ inky.js
@@ -5,3 +5,7 @@
 <p>"What's the old man have so much to do with him for?"</p>
 
 <p>"Striking up a swap or a bargain, I suppose."</p>
+
+<p>"Bargain?&mdash;about what?"</p>
+
+<p>"Why, do ye see, the old man is hard bent after that White Whale, and the devil there is trying to come round him, and get him to swap away his silver watch, or his soul, or something of that sort, and then he'll surrender Moby Dick."</p>
