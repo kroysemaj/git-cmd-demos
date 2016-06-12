23624d53530384374cfa3001e5f5eb6cd52ef918
James York <james@grandcircus.co>
2016-06-12 14:14:51 -0400

    more merge conflicts

--- inky.js
+++ inky.js
@@ -2,10 +2,6 @@
 
 <p>"No doubt, and it's because of his cursed tail; he coils it down, do ye see, in the eye of the rigging."</p>
 
-<p>"What's the old man have so much to do with him for?"</p>
-
-<p>"Striking up a swap or a bargain, I suppose."</p>
-
 <p>"Bargain?&mdash;about what?"</p>
 
 <p>"Why, do ye see, the old man is hard bent after that White Whale, and the devil there is trying to come round him, and get him to swap away his silver watch, or his soul, or something of that sort, and then he'll surrender Moby Dick."</p>
