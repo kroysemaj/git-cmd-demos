a774118bfec1b03ffbbe36c2bafe8c332a88e098
James York <james@grandcircus.co>
2016-06-12 14:07:55 -0400

    commit 1

--- inky.js
+++ inky.js
@@ -1,1 +1,3 @@
-adding some crappy code.
+<p>"He sleeps in his boots, don't he? He hasn't got any hammock; but I've seen him lay of nights in a coil of rigging."</p>
+
+<p>"No doubt, and it's because of his cursed tail; he coils it down, do ye see, in the eye of the rigging."</p>
